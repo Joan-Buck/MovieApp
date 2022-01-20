aebfe34801a16f8020fa294423cbfb65e99fc80d
Gabriel Sitorus <sitorusgg@gmail.com>
2022-01-20 12:38:59 -0800

    delete event handlers and routes

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', e => {
-    // const submitReview = document.querySelector('.reviewSubmitButton')
-    const reviewForm = document.querySelector('.formReview')
+    const reviewForm = document.querySelector('.formReview');
+    // const deleteForm = document.querySelector('.deleteForm');
+    const deleteButton = document.querySelector('.deleteReviewButton')
 
     reviewForm.addEventListener('submit', async(e) => {
         e.preventDefault();
-        // e.stopPropagation();
+        e.stopPropagation();
 
         const movieId = e.target.id;
 
@@ -25,8 +26,9 @@ window.addEventListener('load', e => {
                     reviewForm.style.display = 'none';
 
                     const reviewsDiv = document.querySelector('.reviews');
-                    const newReviewDiv = document.createElement('div');
-                    newReviewDiv.setAttribute('class', 'reviewCard');
+                    const reviewCard = document.createElement('div');
+                    reviewCard.setAttribute('id', `reviewCard_${data.review}`)
+                    reviewCard.setAttribute('class', 'reviewCard');
 
                     const reviewUsername = document.createElement('p');
                     reviewUsername.innerText = data.username;
@@ -35,9 +37,61 @@ window.addEventListener('load', e => {
                     const reviewContent = document.createElement('p');
                     reviewContent.innerText = content;
 
-                    newReviewDiv.append(reviewUsername, reviewTitle, reviewContent);
-                    reviewsDiv.appendChild(newReviewDiv);
+                    // creating delete button
+                    let deleteDiv = document.createElement('div');
+                    let deleteForm = document.createElement('form');
+                    deleteForm.setAttribute('action', `/movies/reviews/${data.review}/delete`)
+                    deleteForm.setAttribute('method', 'POST')
+                    let deleteButton = document.createElement('button');
+                    deleteButton.innerText = 'Delete'
+                    deleteForm.setAttribute('class', 'deleteForm');
+                    deleteForm.setAttribute('id', `review_${data.review}`);
+
+
+                    reviewCard.append(reviewUsername, reviewTitle, reviewContent, deleteDiv);
+                    deleteDiv.appendChild(deleteForm);
+                    deleteForm.appendChild(deleteButton);
+                    reviewsDiv.appendChild(reviewCard);
                 }
             })
-    })
+    });
+    // deleteForm eventListener
+    // deleteForm.addEventListener('submit', async(e) => {
+    //     e.preventDefault();
+    //     e.stopPropagation();
+
+    //     // review_${:id}
+    //     const reviewId = parseInt(e.target.id.split('_')[1]);
+    //     console.log(reviewId)
+
+    //     const res = await fetch(`/movies/reviews/${reviewId}/delete`, {
+    //         method: 'DELETE'
+    //     })
+    //     const data = await res.json()
+    //         .then(data => {
+    //             if (data.message === 'Success') {
+    //                 const review = document.querySelector(`#reviewCard_${reviewId}`);
+    //                 review.remove();
+    //             }
+    //         })
+    // });
+    deleteButton.addEventListener('click', async(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // review_${:id}
+        const reviewId = (e.target.value)
+        console.log(reviewId)
+
+        const res = await fetch(`/movies/reviews/${reviewId}/delete`, {
+            method: 'DELETE'
+        })
+        const data = await res.json()
+            .then(data => {
+                if (data.message === 'Success') {
+                    const review = document.querySelector(`#reviewCard_${reviewId}`);
+                    review.remove();
+                }
+            })
+    });
 })
